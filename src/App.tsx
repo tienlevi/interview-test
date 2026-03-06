37001f29b788cbd0f3ac7c523ae9f03009a7723d
tienlevi <nguyentrachtien2401@gmail.com>
2026-03-06 23:24:19 +0700

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div
+      className=""
+      style={{ textAlign: "center", fontWeight: "bold", fontSize: 24 }}
+    >
+      Quiz
+    </div>
+  );
+}
+
+export default App;
