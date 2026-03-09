0c88a815f29284ee7ea8dfc603e8582dc16ac682
tienlevi <nguyentrachtien2401@gmail.com>
2026-03-10 01:02:39 +0700

    update: UI components

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,15 +3,16 @@ import styles from "./button.module.css";
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
-  variant?: "red" | "blue" | "black";
+  variant?: "red" | "primary" | "black" | "outline";
   children: ReactNode;
 }
 
-function Button({ className, variant = "red", children, ...props }: Props) {
+function Button({ className, variant = "primary", children, ...props }: Props) {
   const variants = {
     red: styles.btnBgRed,
-    blue: styles.btnBgBlue,
+    primary: styles.btnBgBlue,
     black: styles.btnBgBlack,
+    outline: styles.outline,
   };
 
   return (
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,17 +1,33 @@
+import { HTMLAttributes, ReactNode } from "react";
 import styles from "./input.module.css";
 
-interface Props {
+interface Props extends HTMLAttributes<HTMLInputElement> {
+  value?: string;
+  containerClassName?: string;
   className?: string;
   placeholder?: string;
+  prefixNode?: ReactNode;
 }
 
-function Input({ className = "", placeholder = "Enter..." }: Props) {
+function Input({
+  containerClassName = "",
+  className = "",
+  value,
+  placeholder = "Enter...",
+  prefixNode,
+  ...props
+}: Props) {
   return (
-    <input
-      type="text"
-      className={`${styles.input} ${className}`}
-      placeholder={placeholder}
-    />
+    <div className={`${styles.inputContainer} ${containerClassName}`}>
+      <input
+        value={value}
+        type="text"
+        className={`${styles.input} ${className}`}
+        placeholder={placeholder}
+        {...props}
+      />
+      {prefixNode}
+    </div>
   );
 }
 
